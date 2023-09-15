98e340019ae81fcb51f62b04503a780c44b7fc5e
Iamneo <techadmin@iamneo.ai>
2023-09-15 10:43:48 +0000

    Backup Commit

---
+++ Week3MVC/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿// Models/Player.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace Week3MVC.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Category is required.")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Bidding amount is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Bidding amount must be greater than 0.")]
+        public decimal BiddingAmount { get; set; }
+    }
+}
